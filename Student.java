f4395c2027ebdb30904da4c259ec45a5dc2966bd
kheapancho23-art <kheapancho23@gmail.com>
2026-02-18 20:23:30 +0800

    Refactor comments and improve formatting in Student class

--- Student.java
+++ Student.java
@@ -1,29 +1,32 @@
+
 class Student {
     String studentID;
     String name;
     String course;
 
-    // Constructor to initialize attributes
+    // Constructor
     public Student(String studentID, String name, String course) {
         this.studentID = studentID;
         this.name = name;
         this.course = course;
     }
 
-    // Method to display student details
+    // Method to display student info
     public void displayInfo() {
         System.out.println("Student ID: " + studentID);
         System.out.println("Name: " + name);
         System.out.println("Course: " + course);
-        System.out.println(); // For spacing between objects
+        System.out.println();
     }
+}
 
+public class StudentDemo {
     public static void main(String[] args) {
         // Creating two Student objects
         Student s1 = new Student("2023001", "Maria Santos", "BS Computer Science");
         Student s2 = new Student("2023002", "Juan Dela Cruz", "BS Information Technology");
 
-        // Calling displayInfo() for each object
+        // Calling displayInfo()
         s1.displayInfo();
         s2.displayInfo();
     }
